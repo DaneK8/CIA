9d1b015b37ccf0cff2b007f1f20f4021c8ce320d
Owner <drk05160@sjfc.edu>
2018-11-28 10:55:05 -0500

    Commit

--- dataBaseHelper.java
+++ dataBaseHelper.java
@@ -1,42 +1,69 @@
 package com.example.owner.cia;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class dataBaseHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "CIA.db";
-    public static final String TABLE_NAME = "inventory_t";
-    public static final String COL_1 = "item_id";
-    public static final String COL_2 = "item_name";
-    public static final String COL_3 = "location";
-    public static final String COL_4 = "description";
-    public static final String COL_5 = "condition";
+/**
+ * Created by ProgrammingKnowledge on 4/3/2015.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
 
-    public dataBaseHelper( Context context) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NAME + "(" +COL_1 + "INTEGER PRIMARY KEY AUTOINCREMENT,"+ COL_2 +" TEXT, "+ COL_3 + "TEXT, "+ COL_4 +" TEXT, "+COL_5+" TEXT)");
+        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,SURNAME TEXT,MARKS INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
     }
-    public boolean insertData(String item_name,String description ,String condition) {
+
+    public boolean insertData(String name,String surname,String marks) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,item_name);
-        contentValues.put(COL_4, description);
-        contentValues.put(COL_5, condition);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_4,marks);
         long result = db.insert(TABLE_NAME,null ,contentValues);
         if(result == -1)
             return false;
         else
             return true;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        return res;
+    }
+
+    public boolean updateData(String id,String name,String surname,String marks) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1,id);
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_4,marks);
+        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        return true;
+    }
+
+    public Integer deleteData (String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
     }
 }
